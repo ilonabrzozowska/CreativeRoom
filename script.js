a2bf80ac4cada17cd7ac51439d645dbe180aea0c
Ilona Brzozowska <C:\The Bat!\Mail>
2016-05-21 19:05:34 +0200

    Added colorpicker plugin, added buttons to changing color of the wall and screen

--- script.js
+++ script.js
@@ -10,5 +10,36 @@ $(document).ready(function() {
             el.data("text-original", el.text());
             el.text(el.data("text-swap"));
         }
-});
+    });
+        
+    $('#change-screen-color').ColorPicker({
+        color: '#73D8FA',
+        onShow: function (colpkr) {
+            $(colpkr).fadeIn(500);
+            return false;
+        },
+        onHide: function (colpkr) {
+            $(colpkr).fadeOut(500);
+            return false;
+        },
+        onChange: function (hsb, hex, rgb) {
+            $('.screen').css('background-color', '#' + hex);
+        }
+    });
+    
+    $('#change-wall-color').ColorPicker({
+        color: '#ffffff',
+        onShow: function (colpkr) {
+            $(colpkr).fadeIn(500);
+            return false;
+        },
+        onHide: function (colpkr) {
+            $(colpkr).fadeOut(500);
+            return false;
+        },
+        onChange: function (hsb, hex, rgb) {
+            $('body').css('background-color', '#' + hex);
+        }
+    });
+
 })
