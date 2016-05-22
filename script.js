7351fb63f5f93d88841612129dfbbb24f86794dc
Ilona Brzozowska <C:\The Bat!\Mail>
2016-05-22 13:44:00 +0200

    Added functionality of colorPicker for mobile devices (colorPicker is not working for mobile)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    
     $(".playButton").on("click", function() {
         $('.screen').fadeToggle();
 
@@ -12,34 +13,91 @@ $(document).ready(function() {
         }
     });
         
-    $('#change-screen-color').ColorPicker({
-        color: '#73D8FA',
-        onShow: function (colpkr) {
-            $(colpkr).fadeIn(500);
-            return false;
-        },
-        onHide: function (colpkr) {
-            $(colpkr).fadeOut(500);
-            return false;
-        },
-        onChange: function (hsb, hex, rgb) {
-            $('.screen').css('background-color', '#' + hex);
-        }
-    });
     
-    $('#change-wall-color').ColorPicker({
-        color: '#ffffff',
-        onShow: function (colpkr) {
-            $(colpkr).fadeIn(500);
-            return false;
-        },
-        onHide: function (colpkr) {
-            $(colpkr).fadeOut(500);
-            return false;
-        },
-        onChange: function (hsb, hex, rgb) {
-            $('body').css('background-color', '#' + hex);
-        }
-    });
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+                
+        var blue = $('#blue'),
+            red = $('#red'),
+            yellow = $('#yellow'),
+            green = $('#green'),
+            wallBlue = $('#wallBlue'),
+            wallRed = $('#wallRed'),
+            wallYellow = $('#wallYellow'),
+            wallGreen = $('#wallGreen')
+            ;
+        
+        $('#change-screen-color').click(function() {
+            $('.color-screen-buttons').slideToggle('fast', function() {
+                $('.color-screen-button').css('display', 'inline-block');
+                $('.screen-text').css('display', 'block');
+            })
+            
+            blue.click(function() {
+                $('.screen').css('background-color', blue.css('background-color'));
+            });
+            red.click(function() {
+                $('.screen').css('background-color', red.css('background-color'));
+            });
+            yellow.click(function() {
+                $('.screen').css('background-color', yellow.css('background-color'));
+            });
+            green.click(function() {
+                $('.screen').css('background-color', green.css('background-color'));
+            });        
+        })
+        
+        $('#change-wall-color').click(function() {          
+            $('.color-wall-buttons').slideToggle('fast', function() {
+                $('.color-wall-button').css('display', 'inline-block');
+                $('.wall-text').css('display', 'block');
+            })
+            
+            wallBlue.click(function() {
+                $('body').css('background-color', wallBlue.css('background-color'));
+            });
+            wallRed.click(function() {
+                $('body').css('background-color', wallRed.css('background-color'));
+            });
+            wallYellow.click(function() {
+                $('body').css('background-color', wallYellow.css('background-color'));
+            });
+            wallGreen.click(function() {
+                $('body').css('background-color', wallGreen.css('background-color'));
+            });        
+        })
+    }
+    else {
+        $('#change-screen-color').ColorPicker({
+            color: '#73D8FA',
+            onShow: function (colpkr) {
+                $(colpkr).fadeIn(500);
+                return false;
+            },
+            onHide: function (colpkr) {
+                $(colpkr).fadeOut(500);
+                return false;
+            },
+            onChange: function (hsb, hex, rgb) {
+                $('.screen').css('background-color', '#' + hex);
+            }
+        });
+    
+        $('#change-wall-color').ColorPicker({
+            color: '#ffffff',
+            onShow: function (colpkr) {
+                $(colpkr).fadeIn(500);
+                return false;
+            },
+            onHide: function (colpkr) {
+                $(colpkr).fadeOut(500);
+                return false;
+            },
+            onChange: function (hsb, hex, rgb) {
+                $('body').css('background-color', '#' + hex);
+            }
+        });
+    }
+    
+    
 
 })
